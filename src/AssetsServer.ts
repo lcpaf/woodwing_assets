9744182ebf24a17c7c216f408214e87f4fde1bbc
Luis Ferreira <lfe@propublish.de>
2020-08-04 10:42:05 +0200

    [feat] search and move implementations

--- src/AssetsServer.ts
+++ src/AssetsServer.ts
@@ -8,29 +8,65 @@ export class AssetsServer {
     private readonly config: AssetsConfig;
     private authToken: string | null;
 
+    public readonly FOLDER_REPLACE_POLICY_AUTO_RENAME = 'AUTO_RENAME';
+    public readonly FOLDER_REPLACE_POLICY_MERGE = 'MERGE';
+    public readonly FOLDER_REPLACE_POLICY_THROW_EXCEPTION = 'THROW_EXCEPTION';
+
+    public readonly FILE_REPLACE_POLICY_AUTO_RENAME = 'AUTO_RENAME';
+    public readonly FILE_REPLACE_POLICY_OVERWRITE = 'OVERWRITE';
+    public readonly FILE_REPLACE_POLICY_OVERWRITE_IF_NEWER = 'OVERWRITE_IF_NEWER';
+    public readonly FILE_REPLACE_POLICY_REMOVE_SOURCE = 'REMOVE_SOURCE';
+    public readonly FILE_REPLACE_POLICY_THROW_EXCEPTION = 'THROW_EXCEPTION';
+    public readonly FILE_REPLACE_POLICY_DO_NOTHING = 'DO_NOTHING';
+
     constructor(config: AssetsConfig) {
         this.config = config;
         this.authToken = '';
     }
 
-    public authenticate() {
-        const _this = this;
+    public search = (
+        q: string,
+        start: number = 0,
+        num: number = 50,
+        sort: string = 'assetCreated-desc',
+        metadataToReturn: string = 'all',
+        facets: string = '',
+        format: string = 'format',
+        appendRequestSecret: boolean = false,
+        returnHighlightedText: boolean = true
+    ) => {
+        return this.get('/services/search', {
+            q: q,
+            start: start,
+            num: num,
+            sort: sort,
+            metadataToReturn: metadataToReturn,
+            facets: facets,
+            format: format,
+            appendRequestSecret: appendRequestSecret,
+            returnHighlightedText: returnHighlightedText,
+        })
+    };
 
-        return new Promise((resolve, reject) => {
-            _this.callSecondary('/services/apilogin', 'POST', {
-                username: _this.config.username,
-                password: _this.config.password,
-            }).then((data: AssetsLogin) => {
-                if (data.loginSuccess) {
-                    _this.authToken = data.authToken;
-                    return resolve(data);
-                }
-                return reject(data.loginFaultMessage);
-            }).catch((err: Error) => {
-                return reject(err);
-            })
-        });
-    }
+    public move = (
+        source: string,
+        target: string,
+        folderReplacePolicy: string = this.FOLDER_REPLACE_POLICY_AUTO_RENAME,
+        fileReplacePolicy: string = this.FILE_REPLACE_POLICY_AUTO_RENAME,
+        filterQuery: string = '',
+        flattenFolders: boolean = false,
+        async: boolean = false
+    ) => {
+        return this.post('services/move', {
+            source: source,
+            target: target,
+            folderReplacePolicy: folderReplacePolicy,
+            fileReplacePolicy: fileReplacePolicy,
+            filterQuery: filterQuery,
+            flattenFolders: flattenFolders,
+            async: async
+        })
+    };
 
     public get = (service: string, form: object = {}) => this.call(service, 'GET', form);
 
@@ -63,7 +99,26 @@ export class AssetsServer {
         });
     }
 
-    private callSecondary(service: string, method: string, form: object = {}) {
+    private authenticate = () => {
+        const _this = this;
+
+        return new Promise((resolve, reject) => {
+            _this.callSecondary('/services/apilogin', 'POST', {
+                username: _this.config.username,
+                password: _this.config.password,
+            }).then((data: AssetsLogin) => {
+                if (data.loginSuccess) {
+                    _this.authToken = data.authToken;
+                    return resolve(data);
+                }
+                return reject(data.loginFaultMessage);
+            }).catch((err: Error) => {
+                return reject(err);
+            })
+        });
+    }
+
+    private callSecondary = (service: string, method: string, form: object = {}) => {
         const _this = this;
         return new Promise((resolve: any, reject: any) => {
 
@@ -113,7 +168,7 @@ export class AssetsServer {
         });
     }
 
-    private static parseBody(result: any) {
+    private static parseBody = (result: any) => {
         try {
             const body = (result.body) ? result.body : result;
             return (typeof body === 'string') ? JSON.parse(body) : body;
